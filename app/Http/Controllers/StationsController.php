4a4cd6308bd60c06400579b99133fb06236ebb39
ciprian manea <ciprian.manea@netrom.live>
2024-05-22 17:49:51 +0300

    added station details functionality

--- app/Http/Controllers/StationsController.php
+++ app/Http/Controllers/StationsController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\FavouriteStations;
+use App\Models\Plug;
+use App\Models\Review;
 use App\Models\Station;
 use App\Models\Users;
 use Illuminate\Http\JsonResponse;
@@ -13,16 +15,29 @@ class StationsController extends Controller
 {
     /**
      * @param Request $request
+     * * @param integer $id
      * @return JsonResponse
      * @throws \Illuminate\Validation\ValidationException
      */
-    public function index(Request $request)
+    public function index(Request $request, $id = null)
+    {
+        $station = Station::where('id', $id)->first();
+
+        $response_data['data'] = $station;
+        return response()->json($response_data);
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function create(Request $request)
     {
         $stations = $request->stations;
 
         foreach ($stations as $newStation) {
             $station = Station::where('public_id', $newStation['publicId'])->first();
-
 
             if (!$station) {
                 $station = new Station();
@@ -37,15 +52,50 @@ class StationsController extends Controller
                 $station->latitude = floatval($newStation['latitude']);
                 $station->longitude = floatval($newStation['longitude']);
                 $station->is_public = $newStation['isPublic'];
+                $station->rating = $newStation['rating'] ?? 0;
+                $station->rating_count = $newStation['ratingCount'] ?? 0;
                 $station->user_id = null;
 
                 $station->save();
-            }
+
+                foreach ($newStation['plugs'] as $newPlug) {
+                    for($i = 0; $i < $newPlug['count']; $i++) {
+                        $plug = new Plug();
+
+                        $plug->station_id = $station->id;
+                        $plug->type = $newPlug['type'];
+                        $plug->kw_power = $newPlug['kwPower'];
+                        $plug->cost_per_kw = $newPlug['costPerKw'];
+                        $plug->status = $newPlug['status'];
+
+                        $plug->save();
+
+                    }
+                }
+
+                foreach ($newStation['reviews'] as $newReview) {
+                    $review = new Review();
+
+                    $review->comment = $newReview['comment'];
+                    $review->rating = $newReview['rating'];
+                    $review->reviewer_name = $newReview['reviewerName'] ?? null;
+                    $review->reviewer_photo = $newReview['reviewerPhoto'] ?? null;
+                    $review->is_public_reviewer = $newReview['isPublicReviewer'] ?? false;
+                    $review->published_at = $newReview['publishedAt'] ?? null;
+                    $review->station_id = $station->id;
+                    $review->user_id = $newReview['userId'] ?? null;
+
+                    $review->save();
+                }
+            };
+
         }
         $returnStations = Station::all();
         $returnStations->transform(function($station) {
             $station->latitude = floatval($station->latitude);
             $station->longitude = floatval($station->longitude);
+            $station->rating = floatval($station->rating);
+            $station->rating_count = intval($station->rating_count);
             return $station;
         });
 
